8c32aea8c30e86ba089274decbfe3ea55e02c611
Aleksander <aleksander.vestlund@hotmail.com>
2023-10-27 16:42:31 +0200

    Refactor ocr_analyse

--- src/core/admin/ocr_analyse.py
+++ src/core/admin/ocr_analyse.py
@@ -2,9 +2,13 @@ import pytesseract
 from PIL import Image
 
 
+def export_image_text(image_path: str) -> str:
+    image = Image.open(image_path)
+    return pytesseract.image_to_string(image, lang="nor")
+
+
 def main() -> None:
-    image = Image.open("src/receipts/receipt.jpg")
-    print(pytesseract.image_to_string(image, lang="nor"))
+    print(export_image_text("src/receipts/receipt.jpg"))
 
 
 if __name__ == "__main__":
