8421bd2735f349840f924c4e90f7d4112645cdd3
Aleksander <aleksander.vestlund@hotmail.com>
2023-10-28 10:23:21 +0200

    Fix small bugs

--- src/core/ocr_analyse.py
+++ src/core/ocr_analyse.py
@@ -22,7 +22,7 @@ STORE_DICT = {
 
 
 def _export_image_text(image_path: str) -> str:
-    image = Image.open(image_path)
+    image = Image.open(f"src/receipts/{image_path}")
     return pytesseract.image_to_string(image, lang="nor").lower()
 
 
@@ -47,7 +47,7 @@ def _make_gpt_prompt(image_path: str) -> str:
     for i, line in enumerate(lines):
         if not start_index and line.startswith(store.start_text):
             start_index = i + 1
-        elif not end_index and line.startswith(store.end_text):
+        elif line.startswith(store.end_text):
             end_index = i
             break
 
@@ -55,11 +55,18 @@ def _make_gpt_prompt(image_path: str) -> str:
 
 
 def make_gpt_request(image_path: str) -> list[dict[str, str | int]]:
+    # message_prefix = (
+    #     "Lag et JSON-datasett fra følgende matvarer. Inkluder matvare, "
+    #     "antall (standard=1), vekt (standard=N/A) og kategori (kjøleskap/"
+    #     "tørrvare/fryser). Ignorer pris. Rett opp i skrivefeil og generaliser "
+    #     "navnet på matvaren."
+    # )
     message_prefix = (
-        "Lag et JSON-datasett fra følgende matvarer. Inkluder matvare, "
-        "antall (standard=1), vekt (standard=N/A) og kategori (kjøleskap/"
-        "tørrvare/fryser). Ignorer pris. Rett opp i skrivefeil og generaliser "
-        "navnet på matvaren."
+        "Lag et JSON-datasett fra følgende matvarer. Svaret skal være på "
+        "formatet {'matvare': {'antall': int, 'vekt': str, 'kategori': str}, "
+        "...}. Defaultverdi for antall skal være 1, vekt skal være 'N/A' og "
+        "kategori skal være innenfor kjøleskap, tørrvare eller fryser. Rett "
+        "opp i skrivefeil og generaliser navnet på matvaren."
     )
     prompt = _make_gpt_prompt(image_path)
 
@@ -70,16 +77,15 @@ def make_gpt_request(image_path: str) -> list[dict[str, str | int]]:
     )
 
     result = completion.choices[0].message.content  # type: ignore
-    result = ast.literal_eval(result[result.find("[") : result.rfind("]") + 1])
-    print(result)
+    result = ast.literal_eval(result[result.find("{") : result.rfind("}") + 1])
     return result
 
 
 def main() -> None:
     test_receipts = {
-        "rema": "src/receipts/rema_1000.jpg",
-        "obs": "src/receipts/coop_obs.jpeg",
-        "extra": "src/receipts/coop_extra.jpg",
+        "rema": "rema_1000.jpg",
+        "obs": "coop_obs.jpeg",
+        "extra": "coop_extra.jpg",
     }
     print(
         timeit.timeit(
